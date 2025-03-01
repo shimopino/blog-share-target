0286daf962b21b873a2952eed1ef7496ef153fd9
Keisuke Shimokawa <shimoke4869@gmail.com>
2025-03-01 21:29:15 +0900

    feat: deploy-time-buildを使用したS3/CloudFrontデプロイ設定の追加
    - deploy-time-buildパッケージを依存関係に追加
    - APIスタックにS3バケットとCloudFrontディストリビューションを設定
    - ウェブアプリの自動ビルドとデプロイメントを実装
    - CloudFront出力URLを追加
    - SPAルーティングとHTTPS強制のための設定を導入

--- packages/api/lib/api-stack.ts
+++ packages/api/lib/api-stack.ts
@@ -3,8 +3,12 @@ import * as cdk from "aws-cdk-lib";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 import type { Construct } from "constructs";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { NodejsBuild } from "deploy-time-build";
 
 export class ApiStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -70,10 +74,67 @@ export class ApiStack extends cdk.Stack {
 			}),
 		);
 
+		// S3バケットの作成
+		const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
+			removalPolicy: cdk.RemovalPolicy.DESTROY, // 開発環境用
+			autoDeleteObjects: true, // 開発環境用
+			blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+		});
+
+		// CloudFront ディストリビューションの作成
+		const distribution = new cloudfront.Distribution(this, "Distribution", {
+			defaultBehavior: {
+				origin: new origins.S3StaticWebsiteOrigin(websiteBucket),
+				viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+				allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD,
+				cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD,
+			},
+			additionalBehaviors: {
+				"/api/*": {
+					origin: new origins.RestApiOrigin(api),
+					viewerProtocolPolicy:
+						cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+					allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+					cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD,
+					cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED, // APIレスポンスはキャッシュしない
+					originRequestPolicy:
+						cloudfront.OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER,
+				},
+			},
+			defaultRootObject: "index.html",
+			errorResponses: [
+				{
+					httpStatus: 404,
+					responseHttpStatus: 200,
+					responsePagePath: "/index.html", // SPAルーティング用
+				},
+			],
+		});
+
+		// deploy-time-buildを使ったウェブアプリのビルド
+		new NodejsBuild(this, "WebBuilder", {
+			assets: [
+				{
+					path: path.join(__dirname, "../../../packages/web"),
+				},
+			],
+			buildCommands: ["npm install && npm run build"],
+			workingDirectory: ".",
+			outputSourceDirectory: "build/client",
+			destinationBucket: websiteBucket,
+			distribution: distribution,
+		});
+
 		// CDK出力の定義
 		new cdk.CfnOutput(this, "ApiEndpoint", {
 			value: api.url,
 			description: "API Gateway endpoint URL",
+		});
+
+		// CloudFront URL出力の定義
+		new cdk.CfnOutput(this, "WebsiteUrl", {
+			value: `https://${distribution.distributionDomainName}`,
+			description: "Website URL",
 		});
 	}
 }
